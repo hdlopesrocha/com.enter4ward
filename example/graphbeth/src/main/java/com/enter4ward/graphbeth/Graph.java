f066a5d8e23b3aaa59537a7b565566332161a2b0
Henrique Rocha <hdlopesrocha91@gmail.com>
2015-10-03 18:16:28 +0000

    [ADD] graphbeth cycle detection

--- example/graphbeth/src/main/java/com/enter4ward/graphbeth/Graph.java
+++ example/graphbeth/src/main/java/com/enter4ward/graphbeth/Graph.java
@@ -1,51 +1,57 @@
 package com.enter4ward.graphbeth;
 
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Set;
 import java.util.TreeMap;
 
 public class Graph {
 
-	private Map<Alternative, Map<Alternative,Judgement>> transitions;
-	
-	public Graph(List<Judgement> judgements){
-		this.transitions = new TreeMap<Alternative, Map<Alternative,Judgement>>();
+	private Map<Alternative, Map<Alternative, Judgement>> fromTransitions;
+
+	public Graph(List<Judgement> judgements) {
+		this.fromTransitions = new TreeMap<Alternative, Map<Alternative, Judgement>>();
 		for (Judgement j : judgements) {
-			Map<Alternative,Judgement> ts = transitions.get(j.getFrom());
+			Map<Alternative, Judgement> ts = fromTransitions.get(j.getFrom());
 			if (ts == null) {
-				ts = new TreeMap<Alternative,Judgement>();
-				transitions.put(j.getFrom(), ts);
+				ts = new TreeMap<Alternative, Judgement>();
+				fromTransitions.put(j.getFrom(), ts);
 			}
-			ts.put(j.getTo(),j);
+			ts.put(j.getTo(), j);
 		}
 	}
-		
-	public Judgement get(Alternative from, Alternative to){
-		Map<Alternative, Judgement> a = transitions.get(from);
-		if(a!=null){
+
+	public Judgement get(Alternative from, Alternative to) {
+		Map<Alternative, Judgement> a = fromTransitions.get(from);
+		if (a != null) {
 			return a.get(to);
 		}
 		return null;
 	}
+
+	public Collection<Judgement> getFrom(Alternative a) {
+		return fromTransitions.get(a).values();
+	}
 	
-	
-	private boolean hasCycle(Alternative current, Set<Alternative> processing,
-			Map<Alternative, Map<Alternative,Judgement>> transitions, Set<Alternative> visited) {
-		Map<Alternative,Judgement> ts = transitions.get(current);
+	public Collection<Alternative> getAlternatives(){
+		return fromTransitions.keySet();
+	}
+
+	private boolean hasCycle(Alternative current, Set<Alternative> processing, Set<Alternative> visited) {
+		Collection<Judgement> ts = getFrom(current);
 		visited.add(current);
 		processing.add(current);
 		boolean ans = false;
 
 		if (ts != null) {
-			for (Judgement j : ts.values()) {
+			for (Judgement j : ts) {
 				if (processing.contains(j.getTo())) {
 					return true;
 				}
 				if (!visited.contains(j.getTo())) {
-					ans |= hasCycle(j.getTo(), processing, transitions, visited);
+					ans |= hasCycle(j.getTo(), processing, visited);
 					if (ans) {
 						break;
 					}
@@ -56,21 +62,20 @@ public class Graph {
 
 		return ans;
 	}
-	
+
 	public boolean hasCycle() {
 		boolean ans = false;
 		Set<Alternative> visited = new HashSet<Alternative>();
-		for (Entry<Alternative, Map<Alternative, Judgement>> a : transitions.entrySet()) {
+		for (Alternative a : getAlternatives()) {
 			Set<Alternative> processing = new HashSet<Alternative>();
 			if (!visited.contains(a)) {
-				ans |= hasCycle(a.getKey(), processing, transitions, visited);
+				ans |= hasCycle(a, processing, visited);
 				if (ans) {
 					break;
 				}
 			}
 		}
-
 		return ans;
 	}
-	
+
 }
