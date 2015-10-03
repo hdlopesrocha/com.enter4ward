4dd9d82361f905932b3865b5dfedcd3b4969fe08
Henrique Rocha <hdlopesrocha91@gmail.com>
2015-10-03 18:49:34 +0000

    [ADD] graph improvement

--- example/graphbeth/src/main/java/com/enter4ward/graphbeth/Criteria.java
+++ example/graphbeth/src/main/java/com/enter4ward/graphbeth/Criteria.java
@@ -65,6 +65,12 @@ public class Criteria {
 					solver.composeEquation(t1, -1);
 					System.out.print("\t");
 					solver.createEquation(ConstraintType.EQ, 0);
+					
+					System.out.print(" && ");
+					solver.composeEquation(f1, -1);
+					solver.composeEquation(t1, 1);
+					System.out.print("\t");
+					solver.createEquation(ConstraintType.EQ, 0);
 				} else if (j1.getLower() <= j1.getUpper()) {
 					solver.composeEquation(f1, -1);
 					solver.composeEquation(t1, 1);
